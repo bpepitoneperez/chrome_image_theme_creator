fc1251bbc8a94f03657f84cca79d2a61a5c89c37
bryan <brryann21@gmail.com>
2024-07-19 19:32:17 -0600

    cleaned up comments

--- chromeImageThemeCreator.py
+++ chromeImageThemeCreator.py
@@ -1,50 +1,45 @@
-# Importing Image class from PIL module 
 from PIL import Image, ImageEnhance
 import zipfile
 import os
  
-# Opens a image in RGB mode 
+# Opens original image
 original = Image.open(r"original.jpg")
 
-bg = original.resize((2560, 1440))
+width = 2560
+height = 1440
+
+#Resize to seleceted resolution
+bg = original.resize((width, height))
 bg.save("bg.png")
  
-# Size of the image in pixels (size of original image) 
-# (This is not mandatory) 
-width, height = bg.size 
- 
-# Setting the points for frame 
+# Create frame
 frameLeft = 0
 frameTop = 0
 frameRight = width
 frameBottom = 45
- 
-# Cropped image of above dimension
+
 frameImage = bg.crop((frameLeft, frameTop, frameRight, frameBottom))
 frameImage.save("frame.png")
 
 
-# Setting the points for toolbar 
+# Create toolbar
 toolbarLeft = 0
 toolbarTop = frameBottom
 toolbarRight = width
 toolbarBottom = frameBottom + 120
- 
-# Cropped image of above dimension
+
 toolbarImage = bg.crop((toolbarLeft, toolbarTop, toolbarRight, toolbarBottom))
 toolbarImage.save("toolbar.png")
 
-
-# Setting the points for toolbarBG 
+# Create toolbarBG 
 tabBGLeft = 0
 tabBGTop = 0
 tabBGRight = width
 tabBGBottom = 45
- 
-# Cropped image of above dimension
+
 tabBGImage = bg.crop((tabBGLeft, tabBGTop, tabBGRight, tabBGBottom))
 enhancer = ImageEnhance.Brightness(tabBGImage)
-# to reduce brightness by 20%, use factor 0.8
+# Reduce brightness
 tabBGImage = enhancer.enhance(0.8)
 tabBGImage.save("tab_background.png")
 
@@ -57,6 +52,7 @@ zf.write("./toolbar.png")
 zf.write("./tab_background.png")
 zf.close()
 
+# Remove created images
 os.remove("./bg.png")
 os.remove("./frame.png")
 os.remove("./toolbar.png")
